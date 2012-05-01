52eaad6c4ee58e1813cf19145ec633e9c0c8a29b
Sam <samtreweek@gmail.com>
2012-05-01 10:21:28 +0100

    Changed around the spree_slider to be more specific to the site. Some minor amendments, mostly file locations are now in the gem.

--- app/overrides/add_slider_to_layout.rb
+++ app/overrides/add_slider_to_layout.rb
@@ -1,8 +1,19 @@
-Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
-                     :name => 'add_slider',
-                     :insert_after => %q{#header[data-hook]},
-                     :text => %q{
-                     				<div id='spree-slider' data-hook>
-                                  		<%= yield :spree_slider %>
-                                	</div>
-                                })
+#Deface::Override.new(:virtual_path => "spree/home/index",
+#                     :name => "spree_slider",
+#                     :insert_before => "[data-hook='homepage_products']",
+#                     :text  => %q{
+#                     <div id='spree-slider' data-hook="dh_spree_slider">
+#                      <% render 'spree/shared/slider'%>
+#                     </div>
+#                     })
+
+Deface::Override.new(:virtual_path => "spree/home/index",
+                     :name => "spree_slider",
+                     :insert_before => "[data-hook='homepage_products']",
+                     :partial  => 'spree/shared/slider')
+
+#Deface::Override.new(:virtual_path => "spree/home/index",
+#                     :name => "index_news_partial",
+#                     :insert_before => "[data-hook='homepage_products']",
+#                     :partial => "shared/news",
+#                     :disabled => false)
