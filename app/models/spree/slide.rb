7b72fef1d20beaba4844f6b24bf3e3e41a6b6dbd
Sam <samtreweek@gmail.com>
2012-07-31 13:54:23 +0100

    moved model information back into spree_slider

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,5 +1,14 @@
 class Spree::Slide < ActiveRecord::Base
-  has_attached_file :image
-  scope :published, where(:published => true)
   attr_accessible :name, :body, :link_url, :image, :published
+
+  scope :published, where(:published => true)
+
+  if Rails.env.production?
+    has_attached_file :image,
+                      :storage => 's3',
+                      :s3_credentials => Rails.root.join('config', 's3.yml')
+  else
+    has_attached_file :image
+  end
+
 end
